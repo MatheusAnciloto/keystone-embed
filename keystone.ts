353f02a3cd794a91803c00badb51e48bb771df59
MatheusAnciloto <ancilotom@gmail.com>
2021-11-18 07:17:29 -0400

    add postgresql database

--- keystone.ts
+++ keystone.ts
@@ -10,7 +10,7 @@ const Post = list({
 });
 
 export default config({
-  db: { provider: 'sqlite', url: 'file:./app.db' },
+  db: { provider: 'postgresql', url: process.env.DATABASE_URL || ''  },
   experimental: {
     generateNextGraphqlAPI: true,
     generateNodeAPI: true,
